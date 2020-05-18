3f7b1d4d6f59d759689681712f00aa85e72cbe3f
aterriletecno <62068390+aterriletecno@users.noreply.github.com>
2020-05-18 10:24:16 -0400

    FB SDK

--- wp-content/themes/kioskonos/header.php
+++ wp-content/themes/kioskonos/header.php
@@ -82,6 +82,29 @@ $categorias = get_categories($args);
 </head>
 
 <body <?php body_class() ?>>
+
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '{your-app-id}',
+      cookie     : true,
+      xfbml      : true,
+      version    : '{api-version}'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
 	<div class="top-bar">
 		<div class="container">
 			<div class="text-right">
